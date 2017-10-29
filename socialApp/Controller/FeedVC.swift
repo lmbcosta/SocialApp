ace4b952a6a820b4bc1017ec4729add04af0d6a7
lmbcosta <lmbcosta@hotmail.com>
2017-10-29 20:47:16 +0000

    Starting feed tableView ui

--- socialApp/Controller/FeedVC.swift
+++ socialApp/Controller/FeedVC.swift
@@ -11,14 +11,15 @@ import Firebase
 import SwiftKeychainWrapper
 
 class FeedVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // Delegates
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
     @IBAction func signoutBtnPressed(_ sender: Any) {
@@ -33,3 +34,30 @@ class FeedVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 }
+
+// MARK: - TableView DataSource and Delegate
+extension FeedVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as? PostCell {
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
+
+extension FeedVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 300.0
+    }
+}
+
---
+++ socialApp/View/PostCell.swift
@@ -0,0 +1,22 @@
+//
+//  PostCell.swift
+//  socialApp
+//
+//  Created by Luis  Costa on 29/10/17.
+//  Copyright © 2017 Luis  Costa. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var postTextView: UITextView!
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var numberOfLikesLabel: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    
+    
+}
